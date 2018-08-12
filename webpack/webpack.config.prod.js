9d1f76adb70fcb836dd486a1de9ea81deb4a0b86
anzi <anjihhg@gmail.com>
2018-08-12 11:36:58 +0900

    이미지 경로 달라지던 문제 수정

--- webpack/webpack.config.prod.js
+++ webpack/webpack.config.prod.js
@@ -4,7 +4,7 @@ const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 const Webpack = require('webpack');
 const Path = require('path');
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const ExtractSASS = new ExtractTextPlugin('styles/bundle.css');
 
 module.exports = merge(common, {
@@ -12,33 +12,33 @@ module.exports = merge(common, {
   devtool: 'source-map',
   stats: 'errors-only',
   optimization: {
-    minimize: true
+    minimize: true,
   },
   plugins: [
     new Webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify('production')
+      'process.env.NODE_ENV': JSON.stringify('production'),
     }),
     new ExtractTextPlugin({ filename: 'bundle.css' }),
     // compiling mode “scope hoisting”
     new Webpack.optimize.ModuleConcatenationPlugin(),
-    ExtractSASS
+    ExtractSASS,
   ],
   resolve: {
     alias: {
-      '~': Path.resolve(__dirname, '../src')
-    }
+      '~': Path.resolve(__dirname, '../src'),
+    },
   },
   module: {
     rules: [
       {
         test: /\.(js)$/,
         exclude: /(node_modules|bower_components)/,
-        use: 'babel-loader'
+        use: 'babel-loader',
       },
       {
         test: /\.s?css/i,
-        use: ExtractSASS.extract(['css-loader?sourceMap=true&minimize=true', 'sass-loader'])
-      }
-    ]
-  }
+        use: ExtractSASS.extract(['css-loader?sourceMap=true', 'sass-loader']),
+      },
+    ],
+  },
 });
